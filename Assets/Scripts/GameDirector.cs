593095d821e376d9c5d33c6bc57ad4bda347d8e5
alphabstc <51530816+alphabstc@users.noreply.github.com>
2020-10-19 23:10:33 +0800

    upload

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InterfaceController;
+public class GameDirector : System.Object
+{
+	private static GameDirector _instance;
+	public ISceneController CurrentScenceController { get; set; }
+	public static GameDirector GetInstance()
+	{
+		if (_instance == null)
+		{
+			_instance = new GameDirector();
+		}
+		return _instance;
+	}
+}
---
+++ Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ModelController;
+namespace InterfaceController{
+	public interface ISceneController                      
+	{
+		void LoadResources();
+	}
+	public interface IUserAction                           
+	{
+		void MoveBoat();                                   
+		void Restart();                                    
+		void MoveRole(RoleModel role);                                                           
+	}
+}
